4fc1a312575990f51ac307a6b29413d3d97a2b3a
IT25102083 <it25102083@my.sliit.lk>
2026-04-23 20:16:19 +0530

    Create CustomerUser.java

---
+++ src/main/java/com/vehiclerental/model/CustomerUser.java
@@ -0,0 +1,32 @@
+package com.vehiclerental.model;
+
+public class CustomerUser extends User
+{
+    private String customerType;
+
+    public CustomerUser()
+    {
+        super();
+    }
+
+    public CustomerUser(int id, String name, String email, String password, String customerType)
+    {
+        super(id, name, email, password);
+        this.customerType = customerType;
+    }
+
+    public String getCustomerType()
+    {
+        return customerType;
+    }
+
+    public void setCustomerType(String customerType)
+    {
+        this.customerType = customerType;
+    }
+
+    public void bookVehicle()
+    {
+        System.out.println("Customer " + getName() + " is booking a vehicle.");
+    }
+}

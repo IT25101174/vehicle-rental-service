fbdb4f2dcf4cd7d8bfebd9ceae4a52898302e8c7
IT25102083 <it25102083@my.sliit.lk>
2026-05-17 02:11:25 +0530

    Update CustomerUser.java
    add comments

--- src/main/java/com/vehiclerental/model/CustomerUser.java
+++ src/main/java/com/vehiclerental/model/CustomerUser.java
@@ -4,16 +4,24 @@ public class CustomerUser extends User
 {
     private String customerType;
 
+    // Default Constructor
     public CustomerUser()
     {
+        // Calls parent class
         super();
     }
 
+    // Parameterized constructor
     public CustomerUser(int id, String name, String email, String password, String customerType)
     {
+        // Calls parent class
         super(id, name, email, password, "customer");
+
+        // Assign customer type
         this.customerType = customerType;
     }
+
+    //Getter method
 
     public String getCustomerType()
     {

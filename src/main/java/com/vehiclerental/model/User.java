b37ffcf024037536002ba75c856e568f2904aabc
IT25102083 <it25102083@my.sliit.lk>
2026-05-17 01:53:54 +0530

    Update User.java
    update getters & setters

--- src/main/java/com/vehiclerental/model/User.java
+++ src/main/java/com/vehiclerental/model/User.java
@@ -22,21 +22,49 @@ public class User
         this.role = role;
     }
 
+
+
     // Getters & Setters
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public int getId() {
+        return id;
+    }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public String getName() {
+        return name;
+    }
 
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public String getEmail() {
+        return email;
+    }
 
-    public String getRole() { return role; }
-    public void setRole(String role) { this.role = role; }
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 
     // Helper method to write to text file easily
     public String toFileString() {

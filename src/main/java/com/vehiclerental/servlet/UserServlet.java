8e0504f0d87894b6e47f7df079717c9c923bad2d
IT25102083 <it25102083@my.sliit.lk>
2026-05-18 22:56:37 +0530

    Update UserServlet.java

--- src/main/java/com/vehiclerental/servlet/UserServlet.java
+++ src/main/java/com/vehiclerental/servlet/UserServlet.java
@@ -16,21 +16,24 @@ public class UserServlet extends HttpServlet
 {
     UserService service = new UserService();
 
-    // Updated with ServletException for VS Code compilation
+    //Handle get requests
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, jakarta.servlet.ServletException {
+
+        // Get action parameter from url
         String action = request.getParameter("action");
 
-        // Grab the same dynamic path we used earlier!
+        // user data file path
         String dynamicPath = "data/users.txt";
 
-        if ("listUsers".equals(action)) {
-            // 1. Fetch all users from the text file
+        if ("listUsers".equals(action))
+        {
+            // Get all users
             java.util.ArrayList<User> allUsers = service.getUsers(dynamicPath);
 
-            // 2. Attach the list to the request (like putting a package in a delivery truck)
+            // Send user list to jsp
             request.setAttribute("userList", allUsers);
 
-            // 3. Forward the truck into the secure WEB-INF vault to the JSP page
+            // Open userList.jsp
             request.getRequestDispatcher("/WEB-INF/views/userList.jsp").forward(request, response);
         } else if ("edit".equals(action)) {
             int id = Integer.parseInt(request.getParameter("id"));
